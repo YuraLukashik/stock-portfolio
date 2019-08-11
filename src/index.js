885b3998eb12d29b594cb5e89b70b3938aed5ab4
Yura Lukashik <yuralukashik@gmail.com>
2019-08-11 17:39:59 +0300

    ignore js from src/

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { render } from "react-dom";
-// @ts-ignore
-import * as React from "react";
-import { Charts } from "./Charts";
-render(React.createElement(Charts, null), document.getElementById('root-component'));
